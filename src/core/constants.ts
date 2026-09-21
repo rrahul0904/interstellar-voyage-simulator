88b39ebee6a3b61287eedaa4b88e0487cfe4713e
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-20 20:34:50 -0400

    feat: add relativistic simulation core

---
+++ src/core/constants.ts
@@ -0,0 +1,5 @@
+export const C = 299_792_458;
+export const G0 = 9.80665;
+export const LIGHT_YEAR_METERS = 9.4607304725808e15;
+export const DAY_SECONDS = 86_400;
+export const YEAR_SECONDS = 31_557_600;
